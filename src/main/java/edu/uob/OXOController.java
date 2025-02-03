e1586c6d27a929b24774e0e5898ea951d409b171
Lpxxx <lpx@126.com>
2025-02-03 22:14:51 +0000

    初始代码

---
+++ src/main/java/edu/uob/OXOController.java
@@ -0,0 +1,22 @@
+package edu.uob;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+public class OXOController implements Serializable {
+    @Serial private static final long serialVersionUID = 1;
+    OXOModel gameModel;
+
+    public OXOController(OXOModel model) {
+        gameModel = model;
+    }
+
+    public void handleIncomingCommand(String command) throws OXOMoveException {}
+    public void addRow() {}
+    public void removeRow() {}
+    public void addColumn() {}
+    public void removeColumn() {}
+    public void increaseWinThreshold() {}
+    public void decreaseWinThreshold() {}
+    public void reset() {}
+}
