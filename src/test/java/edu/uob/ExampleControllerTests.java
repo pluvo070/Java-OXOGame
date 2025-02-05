6032b1fb2681f76804f915414a37721071184519
pluvo070 <30020422+pluvo070@users.noreply.github.com>
2025-02-05 21:23:10 +0000

    测试文件注释

--- src/test/java/edu/uob/ExampleControllerTests.java
+++ src/test/java/edu/uob/ExampleControllerTests.java
@@ -1,8 +1,13 @@
+// 自动测试的脚本文件
+
 package edu.uob;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
+
+import edu.uob.OXOController;
+import edu.uob.OXOModel;
 import edu.uob.OXOMoveException.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,6 +17,7 @@ class ExampleControllerTests {
   private OXOModel model;
   private OXOController controller;
 
+  // 每个测试用例运行前都会调用此方法，以创建一个3x3的标准棋盘
   // Make a new "standard" (3x3) board before running each test case (i.e. this method runs before every `@Test` method)
   // In order to test boards of different sizes, winning thresholds or number of players, create a separate test file (without this method in it !)
   @BeforeEach
@@ -22,14 +28,17 @@ class ExampleControllerTests {
     controller = new OXOController(model);
   }
 
+  // 发送命令到控制器的工具方法
   // This next method is a utility function that can be used by any of the test methods to _safely_ send a command to the controller
   void sendCommandToController(String command) {
+      // 尝试将命令发送到控制器，如果处理时间过长则超时
       // Try to send a command to the server - call will timeout if it takes too long (in case the server enters an infinite loop)
       // Note: this is ugly code and includes syntax that you haven't encountered yet
       String timeoutComment = "Controller took too long to respond (probably stuck in an infinite loop)";
       assertTimeoutPreemptively(Duration.ofMillis(1000), ()-> controller.handleIncomingCommand(command), timeoutComment);
   }
 
+  // 测试基本的移动和单元格占用功能
   // Test simple move taking and cell claiming functionality
   @Test
   void testBasicMoveTaking() throws OXOMoveException {
@@ -42,11 +51,14 @@ class ExampleControllerTests {
     assertEquals(firstMovingPlayer, controller.gameModel.getCellOwner(0, 0), failedTestComment);
   }
 
+  // 测试基本的胜利检测功能
   // Test out basic win detection
   @Test
   void testBasicWin() throws OXOMoveException {
+    // 获取当前轮到的玩家（他们应该是最终的赢家）
     // Find out which player is going to make the first move (they should be the eventual winner)
     OXOPlayer firstMovingPlayer = model.getPlayerByNumber(model.getCurrentPlayerNumber());
+    // 进行一系列移动
     // Make a bunch of moves for the two players
     sendCommandToController("a1"); // First player
     sendCommandToController("b1"); // Second player
@@ -54,12 +66,15 @@ class ExampleControllerTests {
     sendCommandToController("b2"); // Second player
     sendCommandToController("a3"); // First player
 
+    // A1, A2, A3应当构成第一个玩家的胜利
+    // 检查第一个玩家是否为赢家
     // a1, a2, a3 should be a win for the first player (since players alternate between moves)
     // Let's check to see whether the first moving player is indeed the winner
     String failedTestComment = "Winner was expected to be " + firstMovingPlayer.getPlayingLetter() + " but wasn't";
     assertEquals(firstMovingPlayer, model.getWinner(), failedTestComment);
   }
 
+  // 测试抛出异常的情况
   // Example of how to test for the throwing of exceptions
   @Test
   void testInvalidIdentifierException() throws OXOMoveException {
