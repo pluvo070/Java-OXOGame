e16794e1ec9a408329701941a8d9bc910e9dd3aa
pluvo070 <1478706539@qq.com>
2025-02-05 21:48:16 +0000

    已有元素的行列不可删除

--- src/main/java/edu/uob/OXOModel.java
+++ src/main/java/edu/uob/OXOModel.java
@@ -62,20 +62,18 @@ public class OXOModel implements Serializable {
     }
 
     /* =================== 添加/删除板的长度和高度 ==================*/
-
-    /*  !!!!!! 还没实现: 有内容的行列不可以被删除 !!!!!! */
     public void addColumn(){
         if(gameOver || numberOfColumns == maxNumberOfColumns){ return; }
-        // 方法1: 从第一行开始添加, 每隔[列数+1]个添加一次
+        /* 方法1: 从第一行开始添加, 每隔[列数+1]个添加一次
         for (int i = numberOfColumns-1; i < cells.size(); i += (numberOfColumns+1)) {
             cells.add(i,null);
-        }
-        /*
-        // 方法2: i遍历行数, 每一行末尾+1空位
+        } */
+
+        // 方法2: 遍历行, 计算每一行末尾的索引, +1空位
         for (int i = 0; i < numberOfRows; i++){
             int index = getIndex(i, numberOfColumns); // 在当前行的末尾的索引
             cells.add(index, null);
-        }*/
+        }
         numberOfColumns++;
     }
     public void addRow(){ // 在最后添加新的一行
@@ -86,20 +84,35 @@ public class OXOModel implements Serializable {
         }
         numberOfRows++;
     }
+    /*  !!!!!! 有内容的行列不可以被删除 !!!!!! */
     public void removeColumn(){
         if(gameOver || numberOfColumns == minNumberOfColumns){ return; }
-        // 从最后一个开始删除, 每隔[列数]个元素就删除一次
-        // 从后往前删除, 确保前面即将删除的元素的索引不变
+        /* 先检测这一列的元素是否都为空
+           从第一行开始, 获取每一行末尾元素索引进行检测 */
+        for (int i = 0; i < numberOfRows; i++) {
+            int index = getIndex(i, numberOfColumns - 1);
+            if (cells.get(index) != null) {
+                return;
+            }
+        }
+        /* 方法1: 从最后一个开始删除, 每隔[列数]个元素就删除一次
+           从后往前删除, 确保前面即将删除的元素的索引不变 */
         for(int i = cells.size()-1; i >= numberOfColumns -1; i -= numberOfColumns){
             cells.remove(i);
         }
         numberOfColumns--;
+        /* 方法2: 从最后一行开始, 获取每一行末尾元素索引删除 */
     }
     public void removeRow(){ // 删除最后一行
         if(gameOver || numberOfRows == minNumberOfRows){ return; }
         // 计算需要删除的所有范围, 从最后一个开始往前删除
         int start = getIndex(numberOfRows - 1, 0);
         int end = getIndex(numberOfRows - 1, numberOfColumns - 1);
+        // 先判断这一行是否有元素
+        for (int i = end; i >= start; i--) {
+            if(cells.get(i)!=null){return;}
+        }
+        // 从最后一个开始往前删除
         for (int i = end; i >= start; i--) {
             cells.remove(i);
         }
