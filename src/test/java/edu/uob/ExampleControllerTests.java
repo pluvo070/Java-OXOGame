b5aba2bb65df1411f651f474dd62577258217b18
pluvo070 <1478706539@qq.com>
2025-02-10 17:01:48 +0000

    修正一些警告(不必要)

--- src/test/java/edu/uob/ExampleControllerTests.java
+++ src/test/java/edu/uob/ExampleControllerTests.java
@@ -2,15 +2,17 @@
 
 package edu.uob;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
 
-import edu.uob.OXOController;
-import edu.uob.OXOModel;
+/*import edu.uob.OXOController;*/
+/*import edu.uob.OXOModel;*/
 import edu.uob.OXOMoveException.*;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+/*import org.junit.jupiter.api.Test;*/
 import java.time.Duration;
 
 class ExampleControllerTests {
@@ -41,7 +43,7 @@ class ExampleControllerTests {
   // 测试基本的移动和单元格占用功能
   // Test simple move taking and cell claiming functionality
   @Test
-  void testBasicMoveTaking() throws OXOMoveException {
+  void testBasicMoveTaking() /*throws OXOMoveException*/ {
     // Find out which player is going to make the first move
     OXOPlayer firstMovingPlayer = model.getPlayerByNumber(model.getCurrentPlayerNumber());
     // Make a move
@@ -54,7 +56,7 @@ class ExampleControllerTests {
   // 测试基本的胜利检测功能
   // Test out basic win detection
   @Test
-  void testBasicWin() throws OXOMoveException {
+  void testBasicWin() /*throws OXOMoveException*/ {
     // 获取当前轮到的玩家（他们应该是最终的赢家）
     // Find out which player is going to make the first move (they should be the eventual winner)
     OXOPlayer firstMovingPlayer = model.getPlayerByNumber(model.getCurrentPlayerNumber());
@@ -77,7 +79,7 @@ class ExampleControllerTests {
   // 测试抛出异常的情况
   // Example of how to test for the throwing of exceptions
   @Test
-  void testInvalidIdentifierException() throws OXOMoveException {
+  void testInvalidIdentifierException() /*throws OXOMoveException*/ {
     // Check that the controller throws a suitable exception when it gets an invalid command
     String failedTestComment = "Controller failed to throw an InvalidIdentifierLengthException for command `abc123`";
     // The next lins is a bit ugly, but it is the easiest way to test exceptions (soz)
@@ -87,19 +89,19 @@ class ExampleControllerTests {
   // ------------ 自定义脚本测试其他的异常类 ---------------
   // 1. 检测超出范围
   @Test
-  void testOutsideCellRangeException() throws OXOMoveException {
+  void testOutsideCellRangeException() /*throws OXOMoveException*/ {
     String failedTestComment = "Controller failed to throw an OutsideCellRangeException for command `d1`";
     assertThrows(OutsideCellRangeException.class, ()-> sendCommandToController("d1"), failedTestComment);
   }
   // 2. 检测字符无效
   @Test
-  void testInvalidIdentifierCharacterException() throws OXOMoveException {
+  void testInvalidIdentifierCharacterException() /*throws OXOMoveException*/ {
     String failedTestComment = "Controller failed to throw an InvalidIdentifierCharacterException for command `11`";
     assertThrows(InvalidIdentifierCharacterException.class, ()-> sendCommandToController("11"), failedTestComment);
   }
   // 3. 检测格子已被占用
   @Test
-  void testCellAlreadyTakenException() throws OXOMoveException {
+  void testCellAlreadyTakenException() /*throws OXOMoveException*/ {
     sendCommandToController("a1"); // First player
     sendCommandToController("b1"); // Second player
     String failedTestComment = "Controller failed to throw a CellAlreadyTakenException for command `B1`";
